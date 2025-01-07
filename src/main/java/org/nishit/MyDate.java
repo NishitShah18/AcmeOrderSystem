e9a7d6f058831ef89183322d6f5c8f74b618f378
nishitshah18 <ngshah3@myseneca.ca>
2025-01-07 11:35:48 -0500

    Lab Exercise: Creating Classes

---
+++ src/main/java/org/nishit/MyDate.java
@@ -0,0 +1,30 @@
+package org.nishit;
+
+public class MyDate {
+    int month;
+    int day;
+    int year;
+
+    public MyDate() {
+        this.month = 1;
+        this.day = 1;
+        this.year = 2000;
+    }
+
+    public MyDate(int month,int day,int year) {
+        this.month = month;
+        this.day = day;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return month + "/" + day + "/" + year;
+    }
+
+    public void setDate(int m,int d,int y) {
+        this.month = m;
+        this.day = d;
+        this.year = y;
+    }
+}
