a0a3ba44abaa0f4db562d0b219e08225328d0686
nishitshah18 <ngshah3@myseneca.ca>
2025-01-08 20:14:38 -0500

    Lab Exercise: java.time

--- src/main/java/com/acme/testing/TestOrders.java
+++ src/main/java/com/acme/testing/TestOrders.java
@@ -3,6 +3,8 @@ package com.acme.testing;
 import com.acme.domain.*;
 import com.acme.utils.MyDate;
 
+import java.time.LocalDate;
+
 public class TestOrders {
     public static void main(String[] args) {
 
@@ -32,10 +34,27 @@ public class TestOrders {
         Order birdEradication = new Order(date3, 20000, "Daffy Duck", s3, 1);
         System.out.println("The total bill for: " + birdEradication + " is " + birdEradication.computeTotal());
 
-        //In TestOrders.java pass in a Lambda expression to setRushable which returns true if the orderAmount is over 1500.
-        Order.setRushable((orderDate,orderAmount)->{return orderAmount>1500;});
+        // Change this date to one that is within the last 15 days of today.
+        MyDate hammerDate = new MyDate(12, 31, 2024);
+        Solid hammerType = new Solid("Acme Hammer", 281, 0.3, Good.UnitOfMeasureType.CUBIC_METER, false, 100, 0.25, 0.3);
+        Order hammer = new Order(hammerDate, 10.00, "Wile E Coyote", hammerType, 10);
 
-        System.out.println("Anvil isPriorityOrder: " + anvil.isPriorityOrder()); // true
-        System.out.println("Balloons isPriorityOrder: " + balloons.isPriorityOrder()); // false
+        //In TestOrders.java pass in a Lambda expression to setRushable which returns true if the orderAmount is over 1500.
+        Order.setRushable((orderDate, orderAmount) -> {
+            // Change the lambda implementation to work with LocalDates to determine if an order has been sitting around for
+            // a month or longer (but leave the signature of the isRushable() method alone).
+            // Create a LocalDate object for now.
+            LocalDate now = LocalDate.now();
+            // Create a LocalDate object for the order date.
+            LocalDate orderDatePlus30 = LocalDate.of(orderDate.getYear(), orderDate.getMonth(), orderDate.getDay());
+            // Add one month to the order date.
+            orderDatePlus30 = orderDatePlus30.plusMonths(1);
+            // If the current date is after the order date + one month, it's rushable.
+            return now.isAfter(orderDatePlus30);
+        });
+
+        System.out.println("Anvil isPriorityOrder: " +  anvil.isPriorityOrder());
+        System.out.println("Hammer isPriorityOrder: " +  hammer.isPriorityOrder());
+        System.out.println("Balloons isPriorityOrder: " + balloons.isPriorityOrder());
     }
 }
